a03c4510ca9a73a501b7d9651c06e56872a79a41
Juan David Vega <jdvr.93@hotmail.com>
2017-02-25 13:18:10 +0100

    start project

---
+++ src/main/java/es/juandavidvega/ReactiveTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package es.juandavidvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveTaskManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/es/juandavidvega/ReactiveTaskManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package es.juandavidvega;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveTaskManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

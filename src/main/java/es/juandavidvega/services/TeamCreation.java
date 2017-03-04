e7c796346a25c559c0d414448ceffded999d4edc
Juan David Vega <jdvr.93@hotmail.com>
2017-03-04 13:59:45 +0100

    Remove unused code

--- src/main/java/es/juandavidvega/services/TeamCreation.java
+++ src/main/java/es/juandavidvega/services/TeamCreation.java
@@ -1,32 +1,15 @@
 package es.juandavidvega.services;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.juandavidvega.dto.input.NewTeam;
-import es.juandavidvega.dto.output.OperationStatus;
 import es.juandavidvega.model.Team;
 import es.juandavidvega.repository.write.TeamStorer;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
 @Service
 public class TeamCreation {
-
-    private final  static  Function<? super FluxSink<OperationStatus>, Consumer<Throwable>> errorHandler = fluxSink ->  error -> {
-        fluxSink.next(OperationStatus.ERROR);
-        fluxSink.error(error);
-        fluxSink.complete();
-    };
-
-    private final  static  Function<? super FluxSink<OperationStatus>, Consumer<Team>> successHandler = fluxSink ->  error -> {
-        fluxSink.next(OperationStatus.SUCCESS);
-        fluxSink.complete();
-    };
 
     private final TeamStorer storer;
 

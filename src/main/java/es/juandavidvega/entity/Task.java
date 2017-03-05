0617238e704fb571a2dde5161e2bffacab7eab21
Juan David Vega <jdvr.93@hotmail.com>
2017-03-05 11:13:25 +0100

    Stereotype entities as document

--- src/main/java/es/juandavidvega/entity/Task.java
+++ src/main/java/es/juandavidvega/entity/Task.java
@@ -3,8 +3,10 @@ package es.juandavidvega.entity;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 
+@Document
 public class Task {
 
     @Id
--- src/main/java/es/juandavidvega/entity/Team.java
+++ src/main/java/es/juandavidvega/entity/Team.java
@@ -1,7 +1,9 @@
 package es.juandavidvega.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Team {
 
     @Id

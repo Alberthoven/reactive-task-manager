17b9975ef076e214b33966612f4da266529a02f4
Juan David Vega <jdvr.93@hotmail.com>
2017-03-05 11:14:36 +0100

    Create a query to extract collection with a tailable cursor, this allow an ordered and incremental stream of collection items

--- src/main/java/es/juandavidvega/repository/crud/TaskRepository.java
+++ src/main/java/es/juandavidvega/repository/crud/TaskRepository.java
@@ -1,10 +1,13 @@
 package es.juandavidvega.repository.crud;
 
+import org.springframework.data.mongodb.repository.InfiniteStream;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import es.juandavidvega.entity.Task;
+import reactor.core.publisher.Flux;
 
 public interface TaskRepository extends ReactiveMongoRepository<Task, String> {
 
+    @InfiniteStream
+    public Flux<Task> findWithTailableCursorBy();
 }
